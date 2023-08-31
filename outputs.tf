83ca0b8f67197f0ace32cb20154f931f89a576a5
Charitha <charitha.angage@lvs1.com>
2023-08-31 13:08:08 -0700

    new output

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.servicenow_rg.name
+  value = azurerm_resource_group.servicenow_vm_rg.name
 }
 output "public_ip_address" {
   value = azurerm_linux_virtual_machine.servicenow_vm.public_ip_address
