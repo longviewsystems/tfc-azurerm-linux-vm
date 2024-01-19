8d39fff5b0e1cbf48760702d34a2e97cf6be9158
giridhar-kolli <Giridhar.Kolli@lvs1.com>
2024-01-19 20:57:30 +0000

    feat: mod-updates-2

--- test/fixtures/provider.tf
+++ test/fixtures/provider.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">=0.12"
+  required_version = ">=1.2.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
+      version = ">=3.0.0, <4.0.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~>3.0"
+      version = ">=3.0.0, <4.0.0"
     }
   }
 }
