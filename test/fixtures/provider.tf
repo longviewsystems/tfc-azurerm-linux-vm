e706630fe77557f72cf3ac9af9d6ffdf98f40788
giridhar-kolli <Giridhar.Kolli@lvs1.com>
2024-01-23 17:11:04 +0000

    feat: syntax update

--- test/fixtures/provider.tf
+++ test/fixtures/provider.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">=1.0.11"
+  required_version = "> =1.0.11"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.86.0"
+      version = "> =3.86.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">=3.5.1"
+      version = "> =3.5.1"
     }
   }
 }
